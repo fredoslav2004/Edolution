2ae5b7d9116bed2829ccee49fbf248df95a11603
Eduard Fekete <edko.fek@gmail.com>
2023-12-23 23:53:01 +0100

    [Add] Camera control

---
+++ Edolution/Assets/Scripts/CameraManager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public float zoomSensitivity = 15f;
+    public float minZoom = 5f;
+    public float maxZoom = 20f;
+    [SerializeField] private Camera targetCamera;
+
+    private void Update()
+    {
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        if (scroll != 0f)
+        {
+            // Calculate zoom and clamp it
+            float newSize = Mathf.Clamp(targetCamera.orthographicSize - scroll * zoomSensitivity, minZoom, maxZoom);
+            Vector3 mouseWorldPos = targetCamera.ScreenToWorldPoint(Input.mousePosition) * 2;
+
+            // Calculate position offset based on zoom level change
+            Vector3 direction = (targetCamera.transform.position + mouseWorldPos).normalized;
+            float zoomDelta = targetCamera.orthographicSize - newSize;
+            Vector3 newPosition = targetCamera.transform.position + direction * zoomDelta;
+
+            // Apply new zoom and position
+            targetCamera.orthographicSize = newSize;
+            targetCamera.transform.position = new Vector3(newPosition.x,newPosition.y,targetCamera.transform.position.z);
+        }
+        targetCamera.transform.position += new Vector3(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"),0) * targetCamera.orthographicSize / 30;
+    }
+}
